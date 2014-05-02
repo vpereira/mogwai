1b24d90af212963d1e8dfa46c8503920dad3717e
Victor Pereira <vpereira@suse.de>
2014-05-02 12:05:59 +0200

    added definition for beta5 - not working

---
+++ definitions/sles12_beta5/definition.rb
@@ -0,0 +1,51 @@
+Veewee::Session.declare({
+  :os_type_id  => 'OpenSUSE_64',
+  :cpu_count   => '1',
+  :memory_size => '1024',
+  :disk_size   => '20480',
+  :disk_format => 'VDI',
+  :hostiocache => 'off',
+  :iso_file => 'SLE-12-Server-DVD-x86_64-Beta5-DVD1.iso',
+  :iso_src  => 'http://download.suse.de/install/SLE-12-Server-Beta5/SLE-12-Server-DVD-x86_64-Beta5-DVD1.iso',
+  :iso_md5  => 'c27e3918e9d208cafeb33b968f55694a',
+  :iso_download_timeout => '1000',
+  :boot_wait         => '10',
+  :boot_cmd_sequence => [
+      '<Esc><Enter>',
+      'linux netdevice=enp0s3 netsetup=dhcp install=cd:/',
+      ' lang=en_US',
+      ' autoyast=http://%IP%:8888/autoinst.xml',
+      ' insecure=1 textmode=1',
+      '<Enter>'
+  ],
+  :kickstart_port => "7122",
+  :kickstart_timeout => "10000",
+  :kickstart_file => "autoinst.xml",
+  :ssh_login_timeout => '10000',
+  :ssh_user          => 'vagrant',
+  :ssh_password      => 'vagrant',
+  :ssh_key           => '',
+  :ssh_host_port     => '7222',
+  :ssh_guest_port    => '22',
+  :sudo_cmd     => "echo '%p'|sudo -S sh '%f'",
+  :shutdown_cmd => 'halt',
+  :postinstall_files   => ["postinstall.sh"],
+  :postinstall_timeout => '10000',
+  :hooks => {
+      :before_create => Proc.new do
+        puts "Assembly started at #{Time.now}"
+        puts definition.box.name
+        path = "#{Dir.pwd}/definitions/#{definition.box.name}"
+        $webserver = Thread.new { WEBrick::HTTPServer.new(:Port => 8888, :DocumentRoot => path, :BindAddress=>"127.0.0.1").start }
+      end,
+      :after_boot_sequence => Proc.new do
+         `/usr/bin/curl 'http://127.0.0.1:7122/autoinst.xml'` # we must fire it once, otherwise the process freezes
+      end,
+      :before_postinstall => Proc.new do
+          $webserver.kill
+      end,
+      :after_postinstall => Proc.new do
+          `/usr/bin/VBoxManage controlvm sles12_beta4 poweroff` # hardcoded name/provider sucks! it will break if provider == kvm/libvirt
+      end
+  }
+})
---
+++ definitions/sles12_beta5/postinstall.sh
@@ -0,0 +1,35 @@
+date > /etc/vagrant_box_build_time
+echo 'solver.allowVendorChange = true' >> /etc/zypp/zypp.conf
+echo 'solver.onlyRequires = true' >> /etc/zypp/zypp.conf
+
+zypper --non-interactive rr SUSE-Linux-Enterprise-Server-12-12.0-
+
+if [ -e /etc/zypp/locks ];then
+    rm /etc/zypp/locks
+fi
+echo 'gem: --no-ri --no-rdoc' > /etc/gemrc
+echo -e "\ninstall vagrant key ..."
+mkdir -m 0700 /home/vagrant/.ssh
+cd /home/vagrant/.ssh
+wget --no-check-certificate -O authorized_keys https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub
+chmod 0600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant.users /home/vagrant/.ssh
+
+# update sudoers
+echo -e "\nupdate sudoers ..."
+echo -e "\n# added by veewee/postinstall.sh" >> /etc/sudoers
+echo -e "vagrant ALL=(ALL) NOPASSWD: ALL\n" >> /etc/sudoers
+sed -i -e 's/%sudo\s\+ALL=(ALL:ALL) ALL/%sudo   ALL=(ALL:ALL) NOPASSWD:ALL/' /etc/sudoers
+
+# speed-up remote logins
+echo -e "\nspeed-up remote logins ..."
+echo -e "\n# added by veewee/postinstall.sh" >> /etc/ssh/sshd_config
+echo -e "UseDNS no\n" >> /etc/ssh/sshd_config
+
+zypper --non-interactive addrepo http://download.suse.de/ibs/SUSE:/SLE-12:/GA/standard/ sles12
+zypper --non-interactive in nfs-client rsync
+
+zypper --non-interactive refresh
+
+echo -e "\nall done.\n"
+exit
